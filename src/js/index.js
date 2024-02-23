dd7c51ada4ca74355719845def7b38862e0e9916
jasminMarin <jasmin.marin2021@gmail.com>
2024-02-23 11:10:29 +0100

    docs folder uusgelee

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+require("@babel/polyfill");
+import axios from "axios";
+import Search from "./model/Search";
+
+let search = new Search("pasta");
+search.doSearch()
+        .then(r => console.log(r));
